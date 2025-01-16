9dc9977250e9deb5951a364e600849c140253510
Omnia Fathy <110571447+omnia211@users.noreply.github.com>
2025-01-16 22:26:05 +0200

    Add files via upload

---
+++ Creative landing page/main.js
@@ -0,0 +1,15 @@
+
+
+let img = document.querySelector('.img2');
+let container = document.querySelector('.container')
+
+function phones(phone) {
+    img.src = phone;
+
+}
+
+function colors(color) {
+    container.style.background = color;
+}
+
+
